326f88aac023f1bab0ce16e2f24b4dec29c608a4
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-24 20:24:20 +0300

    task-2_commit

---
+++ tssk-2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numb2 = int.Parse(Console.ReadLine()!);
+
+if (numb1 > numb2) Console.WriteLine($"Max = {numb1}");
+else Console.WriteLine($"Max = {numb2}");
